1edaf88ac7ceef76e0072fff71235a68b619df58
Jaewoong Lee <230@fumire.moe>
2019-11-16 06:28:15 +0900

    Update standalone.py
    use pickle for further

--- Code/standalone.py
+++ Code/standalone.py
@@ -2,6 +2,8 @@
 Standalone
 """
 
+import os
+import pickle
 import pandas
 
 data_directory = "../Data/"
@@ -11,7 +13,7 @@ def get_employee(show=False):
     """
     Get employee information.
 
-    Last modified at 2019-11-16T06:14:42+0900
+    Get employee information from data directory, and drop useless column. Save this with pickle format. Last modified at 2019-11-16T06:26:29+0900
 
     Args:
         show (bool): when this is true, show the data before returning
@@ -20,12 +22,23 @@ def get_employee(show=False):
         DataFrame which contains employee data
 
     """
-    data = pandas.read_excel(data_directory + "/Employee List.xlsx")
+    _pickle_file = ".employee_data.pkl"
+
+    if os.path.exists(_pickle_file):
+        with open(_pickle_file, "rb") as f:
+            _data = pickle.load(f)
+    else:
+        _data = pandas.read_excel(data_directory + "/Employee List.xlsx")
+
+        _data.drop(columns="Unnamed: 0", inplace=True)
+
+        with open(_pickle_file, "wb") as f:
+            pickle.dump(_data, f)
 
     if show:
-        print(data)
+        print(_data)
 
-    return data
+    return _data
 
 
 if __name__ == "__main__":
