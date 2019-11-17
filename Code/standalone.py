c5d87750cdb23a864efd71936fad783b283a6f5b
Jaewoong Lee <230@fumire.moe>
2019-11-18 07:49:03 +0900

    Update standalone.py
    add general analysis

--- Code/standalone.py
+++ Code/standalone.py
@@ -4,11 +4,31 @@ Standalone
 
 import os
 import pickle
+import time
 import matplotlib
 import matplotlib.pyplot
 import pandas
 
 data_directory = "../Data/"
+figure_directory = "figures/"
+
+_x_limit, _y_limit = 189, 111
+
+
+def current_time():
+    """
+    Get current time.
+
+    Wait a second for avoiding duplication, and get current time for file name. Last modified: 2019-11-18T06:48:21+0900
+
+    Args:
+        None
+
+    Returns:
+        String: the time in the string format
+    """
+    time.sleep(1)
+    return "_" + time.strftime("%m%d%H%M%S")
 
 
 def get_employee_data(show=False):
@@ -50,7 +70,7 @@ def get_general_data(show=False):
     """
     Get general building data.
 
-    Get general information of building. Save this with pickle format. Last modified: 2019-11-18T02:10:51+0900
+    Get general information of building. Save this with pickle format. Last modified: 2019-11-18T07:01:22+0900
 
     Args:
         show (bool): when this is true, show the data information before returning
@@ -67,8 +87,11 @@ def get_general_data(show=False):
         _data = pandas.read_csv(data_directory + "BuildingProxSensorData/csv/bldg-MC2.csv")
 
         _data.columns = list(map(lambda x: x.strip(), _data.columns))
+
         _data_obj = _data.select_dtypes(["object"])
         _data[_data_obj.columns] = _data_obj.apply(lambda x: x.str.strip())
+
+        _data["Date/Time"] = pandas.to_datetime(_data["Date/Time"])
 
         with open(_pickle_file, "wb") as f:
             pickle.dump(_data, f)
@@ -111,6 +134,8 @@ def get_hazium_data(data=None, show=False):
         _data_obj = _data.select_dtypes(["object"])
         _data[_data_obj.columns] = _data_obj.apply(lambda x: x.str.strip())
 
+        _data["Date/Time"] = pandas.to_datetime(_data["Date/Time"])
+
         _data.columns = ["Date/Time", "Hazium Concentration"]
 
         with open(_pickle_file, "wb") as f:
@@ -146,6 +171,8 @@ def get_fixed_prox_data(show=False):
         _data_obj = _data.select_dtypes(["object"])
         _data[_data_obj.columns] = _data_obj.apply(lambda x: x.str.strip())
 
+        _data["timestamp"] = pandas.to_datetime(_data["timestamp"])
+
         with open(_pickle_file, "wb") as f:
             pickle.dump(_data, f)
 
@@ -179,6 +206,8 @@ def get_mobile_prox_data(show=False):
         _data_obj = _data.select_dtypes(["object"])
         _data[_data_obj.columns] = _data_obj.apply(lambda x: x.str.strip())
 
+        _data["timestamp"] = pandas.to_datetime(_data["timestamp"])
+
         with open(_pickle_file, "wb") as f:
             pickle.dump(_data, f)
 
@@ -188,15 +217,67 @@ def get_mobile_prox_data(show=False):
     return _data
 
 
+def draw_mobile_prox_data(verbose=True):
+    """
+    Draw mobile prox data for further analysis.
+
+    Last modified: 2019-11-18T07:47:37+0900
+
+    Args:
+        verbose (bool): Verbosity level
+
+    Returns:
+
+    """
+    if verbose:
+        print("Fetch data...")
+    _data = get_mobile_prox_data()
+
+    if verbose:
+        print("Get date/floor")
+    _dates = sorted(list(set(pandas.to_datetime(_data["timestamp"]).apply(lambda x: x.date()))))
+    _floors = sorted(list(set(_data["floor"])))
+
+    if verbose:
+        print(len(_dates), _dates)
+        print(len(_floors), _floors)
+
+    for date in _dates:
+        date_string = date.strftime("%Y-%m-%d")
+        for floor in _floors:
+            if verbose:
+                print("drawing figure:", date, floor)
+
+            data = _data[(pandas.to_datetime(_data["timestamp"]).apply(lambda x: x.date()) == date) & (_data["floor"] == floor)]
+
+            matplotlib.use("Agg")
+            matplotlib.rcParams.update({"font.size": 30})
+
+            matplotlib.pyplot.figure()
+            matplotlib.pyplot.scatter(data["x"], data["y"], alpha=0.3, s=200, marker="X")
+
+            matplotlib.pyplot.title("Mobile prox Data in " + date_string + " on " + str(floor) + "th Floor")
+            matplotlib.pyplot.xlabel("X")
+            matplotlib.pyplot.ylabel("Y")
+            matplotlib.pyplot.xlim(0, _x_limit)
+            matplotlib.pyplot.ylim(0, _y_limit)
+            matplotlib.pyplot.grid(True)
+
+            fig = matplotlib.pyplot.gcf()
+            fig.set_size_inches(32, 18)
+            fig.savefig(figure_directory + "MobileProxData" + current_time() + ".png")
+
+            matplotlib.pyplot.close()
+    else:
+        if verbose:
+            print("Drawing Done!!")
+
+
 if __name__ == "__main__":
     employee_data = get_employee_data(show=True)
-
     general_data = get_general_data(show=True)
-
     hazium_data = [get_hazium_data(data, True) for data in get_hazium_data()]
-
     fixed_prox_data = get_fixed_prox_data(show=True)
-
     mobile_prox_data = get_mobile_prox_data(show=True)
 
-    print(sorted(list(set(fixed_prox_data["prox-id"]))))
+    draw_mobile_prox_data(verbose=True)
